57961253acd043215d4f6aa583874c5418200d0e
Dx-by-Dy <alsmirnov2004@gmail.com>
2025-07-11 15:24:16 +0300

    Trying solve the problem with int var.

--- helpers/solution.py
+++ helpers/solution.py
@@ -54,28 +54,43 @@ class Solution:
         def heuristic(x: float): return abs(x % 1 - 0.5)
         result_var: Var | None = None
         min_heuristic_value = 1
-        result_val: int | None = None
+        left_bound: Bound | None = None
+        right_bound: Bound | None = None
 
         for var, val in zip(self.value[0], self.value[1]):
-            # if not var.is_general or var.is_conv():
-            #     continue
-            # temp_heuristics = heuristic(val)
-            # if temp_heuristics < min_heuristic_value and temp_heuristics < 0.5 - self.primal_tolerance:
-            #     result_var = var
-            #     min_heuristic_value = temp_heuristics
-            #     result_val = int(val)
-
-            if not var.is_general or var.is_conv() or val % 1 < self.primal_tolerance:
+            if not var.is_general or var.is_conv() or min(val % 1, 1 - val % 1) < self.primal_tolerance:
                 continue
-            result_var = var
-            result_val = int(val)
-            break
+            temp_heuristics = heuristic(val)
+            if temp_heuristics < min_heuristic_value and temp_heuristics < 0.5 - self.primal_tolerance:
+                result_var = var
+                min_heuristic_value = temp_heuristics
+
+                if abs(val - var.lower) <= self.primal_tolerance and not isinf(var.lower) and not isinf(var.upper):
+                    bound = (var.lower + var.upper) // 2
+                    left_bound = Bound(lower=var.lower + 1, upper=bound)
+                    right_bound = Bound(lower=bound + 1, upper=var.upper)
+                elif abs(val - var.upper) <= self.primal_tolerance and not isinf(var.lower) and not isinf(var.upper):
+                    bound = (var.lower + var.upper) // 2 - 1
+                    left_bound = Bound(lower=var.lower, upper=bound)
+                    right_bound = Bound(lower=bound + 1, upper=var.upper - 1)
+                else:
+                    if var.upper - var.lower > 10 and not isinf(var.lower) and not isinf(var.upper):
+                        bound = (var.lower + var.upper) // 2
+                    else:
+                        bound = int(val)
+                    left_bound = Bound(lower=var.lower, upper=bound)
+                    right_bound = Bound(lower=bound + 1, upper=var.upper)
+
+                # print(bound, val, var)
+                # print(left_bound, right_bound)
+
+                break
 
         if result_var is not None:
             return BnBBranch(
                 var=result_var,
-                left_bound=Bound(lower=result_var.lower, upper=result_val),
-                right_bound=Bound(lower=result_val + 1, upper=result_var.upper)
+                left_bound=left_bound,
+                right_bound=right_bound
             )
         else:
-            raise Exception("Can't find the variavle for branching!")
+            raise Exception("Can't find the variavble for branching!")
